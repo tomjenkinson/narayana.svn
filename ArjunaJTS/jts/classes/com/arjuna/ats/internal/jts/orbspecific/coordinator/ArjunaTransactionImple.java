e2257149f64176257ae316e9b8bcf863f03b7327
jhalliday <jhalliday@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2011-02-24 10:55:20 +0000

    Revert backport of JBTM-774
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/branches/JBOSSTS_4_6_1_GA_CP@36677 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- ArjunaJTS/jts/classes/com/arjuna/ats/internal/jts/orbspecific/coordinator/ArjunaTransactionImple.java
+++ ArjunaJTS/jts/classes/com/arjuna/ats/internal/jts/orbspecific/coordinator/ArjunaTransactionImple.java
@@ -1317,23 +1317,18 @@ public class ArjunaTransactionImple extends
 		if ((currentStatus != Status.StatusActive)
 				&& (currentStatus != Status.StatusMarkedRollback))
 		{
-			/*
-			 * If XA compliant then return context even if we're inactive. Otherwise
-			 * throw Unavailable for consistency with other OTS implementations.
-			 */
-
-			if (!XA_COMPLIANT)
+			throw new Unavailable();
+		}
+		else
+		{
+			try
+			{
+				return propagationContext();
+			}
+			catch (Exception e)
+			{
 				throw new Unavailable();
-		}
-
-		try
-		{
-			return propagationContext();
-		}
-		catch (Exception e)
-		{
-			throw new UNKNOWN(e.toString(), ExceptionCodes.UNKNOWN_EXCEPTION,
-				CompletionStatus.COMPLETED_NO);
+			}
 		}
 	}
 
@@ -2374,8 +2369,6 @@ public class ArjunaTransactionImple extends
 	static boolean _propagateTerminator = false;
 
 	static boolean _propagateRemainingTimeout = true;  // OTS 1.2 onwards supported this.
-
-	private static final boolean XA_COMPLIANT = true; // if we ever want to disable this then add an mbean option.
 
 	/**
 	 * @message com.arjuna.ats.internal.jts.orbspecific.coordinator.ipunknown
