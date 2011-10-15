b832f692bb88fe0e0bfab11cefdeaeca5b683185
tomjenkinson <tomjenkinson@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2011-10-15 22:08:45 +0000

    JBTM-916 updated to split out the XidImple into its own class
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/branches/JBOSSTS_4_15_0_Final@37557 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ ArjunaJTA/jta/classes/com/arjuna/ats/internal/jta/transaction/arjunacore/jca/SubordinateXidImple.java
@@ -0,0 +1,48 @@
+package com.arjuna.ats.internal.jta.transaction.arjunacore.jca;
+
+import javax.transaction.xa.Xid;
+
+import com.arjuna.ats.internal.jta.xa.XID;
+import com.arjuna.ats.jta.xa.XATxConverter;
+import com.arjuna.ats.jta.xa.XidImple;
+
+public class SubordinateXidImple extends XidImple {
+	public SubordinateXidImple(Xid xid) {
+		super(xid);
+	}
+
+	/**
+	 * Test equality as being part of the same global transaction
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (_theXid.formatID != XATxConverter.FORMAT_ID) {
+			return super.equals(obj);
+		}
+		boolean toReturn = false;
+		if (obj instanceof SubordinateXidImple) {
+			toReturn = isSameTransaction(((SubordinateXidImple) obj));
+		}
+		return toReturn;
+	}
+
+	/**
+	 * Generate the hash code for the xid, subordinates are diffed on the gtrid
+	 * only.
+	 * 
+	 * @param xid
+	 *            The xid.
+	 * @return The hash code.
+	 */
+	@Override
+	protected int getHash(final XID xid) {
+		if (xid == null) {
+			return 0;
+		}
+		if (_theXid.formatID != XATxConverter.FORMAT_ID) {
+			return super.getHash(xid);
+		}
+		return generateHash(xid.formatID, xid.data, 0, xid.gtrid_length);
+	}
+
+}
--- ArjunaJTA/jta/classes/com/arjuna/ats/internal/jta/transaction/arjunacore/jca/TransactionImporterImple.java
+++ ArjunaJTA/jta/classes/com/arjuna/ats/internal/jta/transaction/arjunacore/jca/TransactionImporterImple.java
@@ -31,7 +31,6 @@
 
 package com.arjuna.ats.internal.jta.transaction.arjunacore.jca;
 
-import java.util.Iterator;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.transaction.xa.XAException;
@@ -39,9 +38,6 @@ import javax.transaction.xa.Xid;
 
 import com.arjuna.ats.arjuna.common.Uid;
 import com.arjuna.ats.internal.jta.transaction.arjunacore.subordinate.jca.TransactionImple;
-import com.arjuna.ats.internal.jta.xa.XID;
-import com.arjuna.ats.jta.xa.XATxConverter;
-import com.arjuna.ats.jta.xa.XidImple;
 
 public class TransactionImporterImple implements TransactionImporter
 {
@@ -201,48 +197,5 @@ public class TransactionImporterImple implements TransactionImporter
 	}
 
 	private static ConcurrentHashMap<SubordinateXidImple, SubordinateTransaction> _transactions = new ConcurrentHashMap<SubordinateXidImple, SubordinateTransaction>();
-	
-	private class SubordinateXidImple extends XidImple {
-		public SubordinateXidImple(Xid xid) {
-			super(xid);
-		}
-
-		/**
-		 * Test equality as being part of the same global transaction
-		 */
-		@Override
-		public boolean equals(Object obj) {
-			if (_theXid.formatID != XATxConverter.FORMAT_ID) {
-				return super.equals(obj);
-			}
-			boolean toReturn = false;
-			if (obj instanceof SubordinateXidImple)
-			{
-				toReturn = isSameTransaction(((SubordinateXidImple)obj));
-			}
-			return toReturn;
-		}
-
-		/**
-		 * Generate the hash code for the xid, subordinates are diffed on the gtrid only.
-		 * 
-		 * @param xid
-		 *            The xid.
-		 * @return The hash code.
-		 */
-		@Override
-		protected int getHash(final XID xid) {
-			if (xid == null) {
-				return 0;
-			}
-			if (_theXid.formatID != XATxConverter.FORMAT_ID) {
-				return super.getHash(xid);
-			}
-			return generateHash(xid.formatID, xid.data, 0,
-					xid.gtrid_length);
-		}
-		
-	}
-
 }
 
