84d206b433851d28ae7ddc243ca98a0c14f0a886
zhfeng <zhfeng@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2012-03-27 08:03:07 +0000

    JBTM-1069 update to work with jboss-as-7.1.1.Final
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/branches/JBOSSTS_4_16@37942 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- XTS/sar/crash-recovery-tests/src/test/java/com/arjuna/qa/junit/BaseCrashTest.java
+++ XTS/sar/crash-recovery-tests/src/test/java/com/arjuna/qa/junit/BaseCrashTest.java
@@ -18,7 +18,7 @@ import org.junit.Assert;
 public class BaseCrashTest
 {
     protected String XTSServiceTest = " -Dorg.jboss.jbossts.xts.servicetests.XTSServiceTestName=@TestName@";
-    protected String BytemanArgs = "-Djboss.modules.system.pkgs=org.jboss.byteman -Dorg.jboss.byteman.transform.all -javaagent:target/test-classes/lib/byteman.jar=script:target/test-classes/scripts/@BMScript@.txt,boot:target/test-classes/lib/byteman.jar,listener:true";
+    protected String BytemanArgs = "-Xms64m -Xmx512m -XX:MaxPermSize=256m -Djboss.modules.system.pkgs=org.jboss.byteman -Dorg.jboss.byteman.transform.all -javaagent:target/test-classes/lib/byteman.jar=script:target/test-classes/scripts/@BMScript@.txt,boot:target/test-classes/lib/byteman.jar,listener:true";
     protected String javaVmArguments;
     protected String testName;
     protected String scriptName;
@@ -33,9 +33,9 @@ public class BaseCrashTest
     public static Archive<?> createTestArchive()
     {
         WebArchive archive = ShrinkWrap.
-                createFromZipFile(WebArchive.class, new File(xtstestWar));
+        createFromZipFile(WebArchive.class, new File(xtstestWar));
         final String ManifestMF = "Manifest-Version: 1.0\n"
-                + "Dependencies: org.jboss.modules,deployment.arquillian-service,org.jboss.msc,org.jboss.jts,org.jboss.xts\n";
+            + "Dependencies: org.jboss.modules,deployment.arquillian-service,org.jboss.msc,org.jboss.jts,org.jboss.xts\n";
         archive.setManifest(new StringAsset(ManifestMF));
 
         return archive;
@@ -85,6 +85,14 @@ public class BaseCrashTest
     {
         String log = "target/log";
 
+        String jbossHome = System.getenv().get("JBOSS_HOME");
+        if(jbossHome == null) {
+            Assert.fail("$JBOSS_HOME not set");
+        }
+        String dir = jbossHome + "/standalone/data/tx-object-store/ShadowNoFileLockStore/defaultStore/XTS/";
+        File objectStore = new File(dir);
+        Assert.assertTrue(checkTxObjectStore(objectStore));
+
         if (testName != null && scriptName != null)
         {
             String logFileName = scriptName + "." + testName;
@@ -109,7 +117,7 @@ public class BaseCrashTest
         config.add("javaVmArguments", javaVmArguments + XTSServiceTest.replace("@TestName@", testClass));
 
         controller.start("jboss-as", config.map());
-        deployer.undeploy("xtstest");
+        //deployer.undeploy("xtstest");
         deployer.deploy("xtstest");
 
         //Waiting for crashing
@@ -131,7 +139,7 @@ public class BaseCrashTest
         controller.kill("jboss-as");
     }
 
-    private boolean deleteDirectory(File path)
+    private boolean deleteDirectory(File path) 
     {
         if (path.exists())
         {
@@ -149,5 +157,25 @@ public class BaseCrashTest
             }
         }
         return (path.delete());
+    }
+
+    private boolean checkTxObjectStore(File objectStore) 
+    {    
+        if(objectStore.exists() && objectStore.isDirectory())
+        {
+            File[] files = objectStore.listFiles();
+            if(files != null) {
+                int i = 0;
+                for(i=0;i<files.length;i++) {
+                    if(files[i].isDirectory()) {
+                        if (checkTxObjectStore(files[i]) == false)
+                            return false;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
