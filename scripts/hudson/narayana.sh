d580c87aa4285a9e11a888b7d7e2a6fd7f42f305
zhfeng <zhfeng@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2012-03-29 09:52:25 +0000

    JBTM-1063 update to add script narayana.sh
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/branches/JBOSSTS_4_16@37964 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

---
+++ scripts/hudson/narayana.sh
@@ -0,0 +1,31 @@
+if [ -z "${WORKSPACE}" ]; then
+  echo "UNSET WORKSPACE"
+  exit -1;
+fi
+
+#BUILD JBOSSTS
+ant -Demma.enabled=true -Dpublican=false jbossall
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+#RUN QA TESTS
+cd qa
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+sed -i TaskImpl.properties -e "s#^COMMAND_LINE_0=.*#COMMAND_LINE_0=${JAVA_HOME}/bin/java#"
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+ant -Ddriver.url=file:///home/hudson/dbdrivers get.drivers
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+ant -f run-tests.xml ci-tests emmareport
+if [ "$?" != "0" ]; then
+	exit -1
+fi
