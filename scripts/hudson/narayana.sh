738aa2f39ae2c0c4b6ace4ff8a4842c8f566cf5f
zhfeng <zhfeng@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2012-04-06 05:29:55 +0000

    JBTM-1063 update narayana.sh to build jboss-as from git and add tests commands for xts unit, interop11, crash recovery.
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/branches/JBOSSTS_4_16@37983 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- scripts/hudson/narayana.sh
+++ scripts/hudson/narayana.sh
@@ -9,7 +9,91 @@ if [ "$?" != "0" ]; then
 	exit -1
 fi
 
+#BUILD JBOSS-AS
+cd ${WORKSPACE}
+rm -rf jboss-as
+git clone git://github.com/jbosstm/jboss-as.git
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+cd jboss-as
+git checkout -t origin/4_16_BRANCH
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+./build.sh clean install
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+#START JBOSS
+export JBOSS_HOME=${WORKSPACE}/jboss-as/build/target/jboss-as-7.1.1.Final
+$JBOSS_HOME/bin/standalone.sh --server-config=../../docs/examples/configs/standalone-xts.xml&
+sleep 10 
+
+#RUN XTS UNIT TESTS
+cd ${WORKSPACE}
+cd XTS
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+ant -Dtesttype=tests-11 -Dsartype=sar-11 install
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+cp xts-install/tests/*ear $JBOSS_HOME/standalone/deployments/
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+sleep 5
+
+export MYTESTIP_1=localhost
+ant -f localjunit/run-tests.xml tests-11
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+#RUN INTEROP11 TESTS
+cp xts-install/interop-tests/interop11.war $JBOSS_HOME/standalone/deployments/
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+sleep 5
+
+ant -f localjunit/run-interop-tests.xml -Dserver.hostname=localhost wstx11-interop-tests
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+#SHUTDOWN JBOSS
+$JBOSS_HOME/bin/jboss-cli.sh --connect command=:shutdown
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+#RUN XTS CRASH RECOVERY TESTS
+cd ${WORKSPACE}
+cd XTS/sar/crash-recovery-tests
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+mvn clean test
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
+java -cp target/classes/ com.arjuna.qa.simplifylogs.SimplifyLogs ./target/log/ ./target/log-simplified
+if [ "$?" != "0" ]; then
+	exit -1
+fi
+
 #RUN QA TESTS
+cd ${WORKSPACE}
 cd qa
 if [ "$?" != "0" ]; then
 	exit -1
