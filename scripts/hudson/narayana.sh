8bc4e5a54212c1c76e64cc56eec3e5ba15dda1b0
paul.robinson <paul.robinson@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2012-05-18 12:48:52 +0000

    Hardcoded JBOSS_HOME to jboss-as-7.2.0.Alpha1-SNAPSHOT as auto-detecting it failed
    git-svn-id: https://anonsvn.jboss.org/repos/labs/labs/jbosstm/branches/JBOSSTS_4_16@38092 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- scripts/hudson/narayana.sh
+++ scripts/hudson/narayana.sh
@@ -41,7 +41,7 @@ if [ "$?" != "0" ]; then
 fi
 
 #START JBOSS
-export JBOSS_HOME="${WORKSPACE}/jboss-as/build/target/jboss-as-*"
+export JBOSS_HOME="${WORKSPACE}/jboss-as/build/target/jboss-as-7.2.0.Alpha1-SNAPSHOT"
 $JBOSS_HOME/bin/standalone.sh --server-config=../../docs/examples/configs/standalone-xts.xml&
 sleep 10 
 
